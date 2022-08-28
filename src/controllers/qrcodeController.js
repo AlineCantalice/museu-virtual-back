c487e48ae8372c2dd61f36640cb31f3afcb9297f
AlineCantalice <alinedecantalice@gmail.com>
2022-08-28 17:38:32 -0300

    recebe html e renderiza a página

--- src/controllers/qrcodeController.js
+++ src/controllers/qrcodeController.js
@@ -1,7 +1,6 @@
 import afterload from 'after-load'
 
 export async function getURL(req, res) {
-    console.log(req.body)
     const html = afterload(req.body.link)
     res.status(200).send(html)
 }
